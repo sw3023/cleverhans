8270b7628379d095c189dd3134eb8e8bffde1c04
Shuhao Wu <sw3023@columbia.edu>
2017-10-30 15:20:35 -0700

    Update ex_cifar10_tf.py

--- examples/ex_cifar10_tf.py
+++ examples/ex_cifar10_tf.py
@@ -38,7 +38,9 @@ def data_cifar10():
     nb_classes = 10
 
     # the data, shuffled and split between train and test sets
-    (X_train, y_train), (X_test, y_test) = cifar10.load_data_local(origin = 'http://localhost/cifar-10-python.tar.gz')
+    #(X_train, y_train), (X_test, y_test) = cifar10.load_data_local(origin = 'http://localhost/cifar-10-python.tar.gz')
+    (X_train, y_train), (X_test, y_test) = cifar10.load_data_local(origin = 'http://localhost/lisa-17-python.tar.gz', num_train_samples = 4183, train_samples = 4183)
+
 
     if keras.backend.image_dim_ordering() == 'th':
         X_train = X_train.reshape(X_train.shape[0], 3, img_rows, img_cols)
